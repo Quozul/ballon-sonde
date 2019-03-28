6dbe2f609add5cb65fed802d9219f92474795254
Erwan <erwan.legloannec@dartybox.com>
2019-03-28 17:52:21 +0100

    Add files via upload

--- Erwan/Piles/Piles.ino
+++ Erwan/Piles/Piles.ino
@@ -1,15 +1,15 @@
 long int reset;
 
-const float R1 = 100.0; // valeur de la première résistance
-const float R2 = 500.0; // valeur de la seconde résistance
-const float a = 2.850; // autonomie totale des piles en Ah
+const float R1 = 300.0; // valeur de la première résistance
+const float R2 = 180.0; // valeur de la seconde résistance
+const float a = 2.500+2.000+2.100+2.500+2.100+2.100; // autonomie totale des piles en Ah
 
 const float coeff = R2 / ( R1 + R2); // calcul du coefficient de réduction
 const float req = (R1 * R2) / ( R1 + R2 ); // calcul de la résistance équivalente
 const float res = 5 / 1024; // calcul la résolution de la borne analogique (V / unité)
 
-const float tensionMax = 6.0; // tension maximale en V
-const float tensionMin = 5.5; // défini la tension minimale en V
+const float tensionMax = 7.2; // tension maximale en V
+const float tensionMin = 6; // défini la tension minimale en V
 
 const int treuilPin = 2; // broche pour activer le treuil
 
@@ -30,6 +30,8 @@ float battery() {
   float t = a / i; // calcul l'autonomie en h
   
   if (reset <= millis()) { // affiche des valeurs toutes les 0.5 secondes
+    Serial.println("");
+    Serial.print(rawcurr, 0); // affiche la tension dans le moniteur de série avec 7 décimales
     Serial.println("");
     Serial.print(u, 7); // affiche la tension dans le moniteur de série avec 7 décimales
     Serial.println("V");
